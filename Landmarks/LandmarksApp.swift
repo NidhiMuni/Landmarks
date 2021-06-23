fc51e07404a4886ef6e086ebb32d252886477f90
Nidhi <nidhi.munikote@outlook.com>
2021-06-22 21:38:11 -0700

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Nidhi Munikote on 6/22/21.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
